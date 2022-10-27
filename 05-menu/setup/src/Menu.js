b56c29857ed7676815b274ec8f6203bfcd50a076
chienvn <chiennguyen1702@gmail.com>
2022-10-28 06:32:05 +0700

    Project: menu

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const {id, title, category, price, img, desc} = menuItem;
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} className='photo' alt={title} />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h5 className='price'>${price}</h5>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Menu;
