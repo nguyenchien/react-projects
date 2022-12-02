4b57edef6f9f3bf275a727ee24f81d2737412cf9
chienvn <chiennguyen1702@gmail.com>
2022-12-02 21:16:35 +0700

    project: tripe - setup useContext

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,42 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  }
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  }
+  const closeSubmenu = () => {
+    setIsSidebarOpen(false);
+  }
+  
+  return (
+    <AppContext.Provider value={{
+      isSidebarOpen,
+      isSubmenuOpen,
+      openSidebar,
+      closeSidebar,
+      openSubmenu,
+      closeSubmenu
+    }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// custom hook
+const useGlobalContext = () => {
+  return useContext(AppContext);
+}
+
+export {AppContext, AppProvider, useGlobalContext}
