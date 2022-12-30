77cf46f21bb852642966b447da0da01214079848
chienvn <chiennguyen1702@gmail.com>
2022-12-31 05:22:09 +0700

    random user: setup

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -10,7 +10,46 @@ import {
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
-  return <h2>random user starter</h2>
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random person');
+  const handleValue = (e) => {
+    console.log(e.target);
+  }
+  return <main>
+    <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img src={defaultImage} className="user-img" alt="" />
+          <p className="user-title">my {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button className="icon" data-lable="name" onMouseOver={handleValue}>
+              <FaUser/>
+            </button>
+            <button className="icon" data-lable="email" onMouseOver={handleValue}>
+              <FaEnvelopeOpen/>
+            </button>
+            <button className="icon" data-lable="age" onMouseOver={handleValue}>
+              <FaCalendarTimes/>
+            </button>
+            <button className="icon" data-lable="street" onMouseOver={handleValue}>
+              <FaMap/>
+            </button>
+            <button className="icon" data-lable="phone" onMouseOver={handleValue}>
+              <FaPhone/>
+            </button>
+            <button className="icon" data-lable="password" onMouseOver={handleValue}>
+              <FaLock/>
+            </button>
+          </div>
+          <button  type='button' className='btn'>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+  </main>
 }
 
 export default App
