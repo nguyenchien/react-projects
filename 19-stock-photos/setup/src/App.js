38dd2c257c0a37a4bb0daae619cf2534c426de2e
chienvn <chiennguyen1702@gmail.com>
2023-01-02 15:49:38 +0700

    stock photo: setup env variable

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-//Access Key: KTrpVjdTpUVAYYfhMS1VMjV7hyPjarBrKcbOEqgxrnA
-//Secret key: unUgwWqFB7dliE4OECglBpQiY_efxYVsj1VfoerBXhU
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
@@ -11,9 +9,9 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
   const fetchImages = async() => {
+    setLoading(true);
+    const url = `${mainUrl}${clientID}`;
     try {
-      setLoading(true);
-      const url = `${mainUrl}?client_id=KTrpVjdTpUVAYYfhMS1VMjV7hyPjarBrKcbOEqgxrnA`;
       const response = await fetch(url);
       const data = await response.json();
       setPhotos(data);
