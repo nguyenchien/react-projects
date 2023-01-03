f9837a2fd227ea23d28b52f3dc5cfe45b8c83bb3
chienvn <chiennguyen1702@gmail.com>
2023-01-03 13:26:57 +0700

    stock photos: optimize code

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
+/*
+  Fix List
+  - remove current scroll code
+  - set default page to 1
+  - setup 2 useEffects
+  - dont't run second on initial render
+  - check for query value
+  - if page 1, fetch images
+  - otherwise, setPage(1)
+  - fix scoll function
+*/
+
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
   
   const fetchImages = async() => {
     setLoading(true);
@@ -34,36 +48,52 @@ function App() {
         }
       });
       setLoading(false);
+      setNewImages(false);
     } catch (error) {
       setLoading(false);
+      setNewImages(false);
       console.log(error);
     }
   }
   
   // handle fetch image on init
   useEffect(() => {
-    fetchImages()
+    fetchImages();
+    console.log('init');
     // eslint-disable-next-line
   }, [page]);
   
-  // handle scroll loading image
+  // handle load more images
   useEffect(() => {
-    const event = window.addEventListener('scroll', ()=>{
-      if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
-        // handle load more images
-        setPage((oldPage)=>{
-          return oldPage + 1;
-        })
-      }
-    });
-    return ()=> {
-      window.removeEventListener('scroll', event);
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
     }
+    if(loading) return;
+    if(!newImages) return;
+    console.log('second');
     // eslint-disable-next-line
-  }, []);
+  }, [newImages])
+  
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      console.log('load new images');
+      setNewImages(true);
+      setPage((oldPage) => oldPage + 1);
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+    return () => window.removeEventListener('scroll', event);
+  }, [])
   
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
     setPage(1);
   }
   
