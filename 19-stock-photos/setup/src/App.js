9c6c45f55b097f07e3dd90d1979041bd6ad6a5df
chienvn <chiennguyen1702@gmail.com>
2023-01-03 11:39:06 +0700

    stock photos: fix search

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -8,7 +8,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [query, setQuery] = useState('');
   
   const fetchImages = async() => {
@@ -25,7 +25,9 @@ function App() {
       const response = await fetch(url);
       const data = await response.json();
       setPhotos((oldPhotos)=>{
-        if (query) {
+        if (query && page === 1 ) {
+          return data.results
+        } else if (query) {
           return [...oldPhotos, ...data.results]
         } else {
           return [...oldPhotos, ...data]
@@ -60,7 +62,7 @@ function App() {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchImages();
+    setPage(1);
   }
   
   return (
