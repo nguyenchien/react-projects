ddf588226856233466817ed6d0cc1a3a4d51c842
chienvn <chiennguyen1702@gmail.com>
2023-01-03 21:28:28 +0700

    dark-mode: setup

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -3,7 +3,21 @@ import data from './data'
 import Article from './Article'
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav className="nav-center">
+        <h1>overreacted</h1>
+        <button className='btn'>toogle</button>
+      </nav>
+      <section className="articles">  
+      {
+        data.map((item, index) => {
+          return <Article key={index} {...item} />
+        })
+      }
+      </section>
+    </main>
+  )
 }
 
 export default App
